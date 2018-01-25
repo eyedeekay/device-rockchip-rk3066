3a1023892d319624fb78133ff55d1b2ede96d346
idk <problemsolver@openmailbox.org>
2018-01-24 21:29:05 -0500

    Got to remember something

--- rk3066.mk
+++ rk3066.mk
@@ -2,6 +2,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/rockchip/rk3066/device.mk)
 PRODUCT_BRAND := rockchip
 PRODUCT_DEVICE := rk3066
-PRODUCT_NAME := lineage_rk3066
+PRODUCT_NAME := rk3066
 PRODUCT_MANUFACTURER := rockchip
 TARGET_PREBUILT_KERNEL := device/rockchip/rk3066/zImage
