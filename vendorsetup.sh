e6b8cdb36d0a49b9c2463c5bab1f61543d6245a1
idk <problemsolver@openmailbox.org>
2018-01-25 23:10:21 -0500

    Not sure it did anything at all, so I'm leaving it for now.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 
-add_lunch_combo lineage_rk3066
-add_lunch_combo lineage_rk3066-userdebug
-add_lunch_combo rk3066
+#Sadd_lunch_combo lineage_rk3066
+#add_lunch_combo lineage_rk3066-userdebug
+#add_lunch_combo rk3066
 add_lunch_combo rk3066-userdebug
